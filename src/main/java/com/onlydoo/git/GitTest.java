eb91fc3e1e39675b273585077956e16597d67029
Onlydo <Onlydo@aaa.com>
2022-08-21 09:42:55 +0800

    second commit

--- src/main/java/com/onlydoo/git/GitTest.java
+++ src/main/java/com/onlydoo/git/GitTest.java
@@ -3,5 +3,6 @@ package com.onlydoo.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!");
+        System.out.println("hello git3!");
     }
 }
