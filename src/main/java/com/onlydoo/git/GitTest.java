78b66df534f8d9bc0d0f1dd087bc289518d83021
Onlydo <Onlydo@aaa.com>
2022-08-21 10:23:09 +0800

    hot-fix commit1

--- src/main/java/com/onlydoo/git/GitTest.java
+++ src/main/java/com/onlydoo/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git!");
         System.out.println("hello git3!");
+        System.out.println("hello git7!");
+        System.out.println("hello git9!");
     }
 }
